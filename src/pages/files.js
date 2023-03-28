f0eed5724b2f041367b6a6a56d4c2f8a248191f3
Alan <aylaj@reed.edu>
2023-03-28 13:38:05 -0700

    not working yet but maybe better???

--- src/pages/files.js
+++ src/pages/files.js
@@ -1,44 +1,59 @@
-import React from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import RemoteTextApi from '../externalApis/remoteTextApi.js'
+import React, { useEffect, useState } from 'react'
 
 const remoteTextApi = new RemoteTextApi()
 
 async function awaitListFiles() {
-	return await remoteTextApi.listFiles()
+	await remoteTextApi.listFiles()
+  .then(filesData => {
+    console.log(filesData)
+    return filesData
+  })
+  .catch(error => {
+    console.log(error.response)
+  })
 }
 
 function getFile(id) {  // placeholder for opening next page to view/edit selected file
   console.log('get file with id='+id)
 }
 
-function printFiles(fileList) {
-  let htmlStr = ''
-  fileList.forEach((file)=>{
-    let name = file['name']
-    let id = file['id']
-    htmlStr += "<button onClick={getFile('"+id+"')}>"+name+"</button><br>"
-  })
-  return htmlStr
+function listFileButtons(jsonList) {
+  if (jsonList.length > 0) {
+    jsonList.forEach((fileJson)=>{
+      let fileName = fileJson['name']
+      let fileId = fileJson['id']
+      //combine? (<button onClick={() => getFile(fileId)}>{fileName}</button>)
+    })
+    return (<p>Doing something here!</p>)
+  } else {
+    console.log('here!')
+    return <p>It doesn't look like you have any files at the moment.</p>
+  }
 }
 
 export default function Files() {
-  awaitListFiles().then(jsonList => {
-    let htmlStr = printFiles(jsonList)
-    if (htmlStr == '') {
-      htmlStr = "<p>It doesn't look like you have any files at the moment.</p>"
-    }
-    text.innerHTML = htmlStr
-  })
+  console.log("await: ", awaitListFiles())
 
-  return (
+  const [listFiles, displayListFiles] = useState({});
+
+  useEffect(() => {
+    awaitListFiles()
+    .then(data => displayListFiles(data))
+  }, [])
+
+  console.log("data? ", listFiles)
+
+  return(
     <>
       <Head>
         <title>Files - RemoteText</title>
       </Head>
       <main className = {styles.main}>
         <div id='text' className={styles.description}>
+          <p>these are some files:</p>
 				</div>
       </main>
     </>
