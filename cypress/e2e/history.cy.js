4677210bfef9628b134333975c15d89285a68f74
cadencorontzos <60051669+cadencorontzos@users.noreply.github.com>
2023-04-09 13:13:22 -0700

    made the test a e2e test and get id from the query string

--- cypress/e2e/history.cy.js
+++ cypress/e2e/history.cy.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import HistoryPage from '../../src/pages/history'
 
-describe('<HistoryPage />', () => {
+describe('History', () => {
 	it('render and displays expected content', () => {
 		cy.intercept('**/api/getHistory', {fixture: 'exampleHistory'}).as('getHistory')
-		cy.mount(<HistoryPage />)
+		cy.visit({
+			url: 'http://localhost:3000/history',
+			method: 'GET',
+			qs: {
+				id: '1'.repeat(32)
+			}
 
+		})
 		cy.get('[id=treeWrapper]').should('be.visible')
 		cy.get('[id=treeWrapper]').within(() => {
 
 			// see the root
 			cy.get('*[class^="rd3t-node historyTree_node__root"]').should('be.visible')
-
 			// see the branch
 			cy.get('*[class^="rd3t-node historyTree_node__branch"]').scrollIntoView().should('be.visible')
 
@@ -24,7 +28,14 @@ describe('<HistoryPage />', () => {
 
 	it('should log the hashes when you press the buttons', () => {
 		cy.intercept('**/api/getHistory', {fixture: 'exampleHistory'}).as('getHistory')
-		cy.mount(<HistoryPage />)
+		cy.visit({
+			url: 'http://localhost:3000/history',
+			method: 'GET',
+			qs: {
+				id: '1'.repeat(32)
+			}
+
+		})
 
 		cy.get('[id=treeWrapper]').should('be.visible')
 		cy.get('[id=treeWrapper]').within(() => {
@@ -34,6 +45,19 @@ describe('<HistoryPage />', () => {
 			cy.get('*[class^="rd3t-node historyTree_node__root"]').get('circle').should('be.visible')
 			cy.get('*[class^="rd3t-node historyTree_node__branch"]').scrollIntoView().get('circle').should('be.visible')
 			cy.get('*[class^="rd3t-leaf-node historyTree_node__leaf"]').should('have.length', 2).get('circle').should('be.visible')
+
+		})
+	})
+
+	it('return 404 when no id is provided', () => {
+		cy.visit({
+			url: 'http://localhost:3000/history',
+			method: 'GET',
+		})
+
+		cy.get('[id=404div]').within(() => {
+
+			cy.get('h1').contains("Something went wrong! Please go home and try again")
 
 		})
 	})
--- src/pages/history.js
+++ src/pages/history.js
@@ -1,25 +1,13 @@
 import RemoteTextApi from '../externalApis/remoteTextApi.js'
 import style from '../styles/historyTree.module.css'
-import React, {useCallback, useState, useEffect} from "react"
+import React, {useCallback, useState, useLayoutEffect} from "react"
+import Link from 'next/link'
 import Tree from 'react-d3-tree';
 
 const remoteTextApi = new RemoteTextApi();
 
-export const useCenteredTree = (defaultTranslate = {x: 0, y: 0}) => {
-	const [translate, setTranslate] = useState(defaultTranslate);
-	const containerRef = useCallback((containerElem) => {
-		if (containerElem !== null) {
-			const {width, height} = containerElem.getBoundingClientRect();
-			setTranslate({x: width / 2, y: height / 2});
-		}
-	}, []);
-	return [translate, containerRef];
-};
 
 function createHistoryTree(commitMap, refMap, rootHash) {
-
-
-
 
 	var historyTree = {
 		name: rootHash.substring(0, 7),
@@ -49,17 +37,18 @@ function createHistoryTree(commitMap, refMap, rootHash) {
 
 
 
-async function getHistoryAndMakeTree() {
-	var hist = await remoteTextApi.getHistory("9bc0e1fd-253c-4c50-b3d1-7c464c3b49d5")
-	if (!hist) {
+async function getHistoryAndMakeTree(id) {
+	if (!id) {
+		return null
+	}
+	var hist = await remoteTextApi.getHistory(id)
 
-		throw new Error
+	// if call fails, go to 404
+	if (!hist) {
+		return null
 	}
 
-	// remove this 
-	hist = testobj
 	var commitMap = new Map();
-
 	var refMap = new Map();
 
 	for (let i = 0; i < hist.commits.length; i++) {
@@ -89,86 +78,107 @@ async function getHistoryAndMakeTree() {
 	return treePromise
 }
 
+// Here is how to extract the hash from pressing on a node
 function logNode(event) {
 	console.log(event.data.properties.hash)
 }
-const renderForeignObjectNode = ({
-	nodeDatum,
-	foreignObjectProps
-}) => (
-	<g>
-		<circle r={30}></circle>
-		{/* `foreignObject` requires width & height to be explicitly set. */}
-		<foreignObject {...foreignObjectProps}>
-			<div style={{x: 200, dy: "2em", border: "1px solid black", backgroundColor: "#dedede"}}>
-				<h3 style={{textAlign: "center"}}>{nodeDatum.name}</h3>
-			</div>
-		</foreignObject>
-	</g>
-);
 
-let testobj = {
-	"commits": [
-		{
-			"hash": "aceaaec23664ae26d76ab66cedfb1206b9c972b1",
-			"parent": null
-		},
-		{
-			"hash": "7c570dce251232eecd2daa6bd81723ef0a1a7590",
-			"parent": "aceaaec23664ae26d76ab66cedfb1206b9c972b1"
-		},
-		{
-			"hash": "7ce70dce251232eecd2daa6bd81723ef0a1a7591",
-			"parent": "7c570dce251232eecd2daa6bd81723ef0a1a7590"
-		},
-		{
-			"hash": "8c570dce251232eecd2daa6bd81723ef0a1a7590",
-			"parent": "aceaaec23664ae26d76ab66cedfb1206b9c972b1"
+// a way to have prettier node names
+// to implement later
+// const renderForeignObjectNode = ({
+// 	nodeDatum,
+// 	foreignObjectProps
+// }) => (
+// 	<g>
+// 		<circle r={30}></circle>
+// 		{/* `foreignObject` requires width & height to be explicitly set. */}
+// 		<foreignObject {...foreignObjectProps}>
+// 			<div style={{x: 200, dy: "2em", border: "1px solid black", backgroundColor: "#dedede"}}>
+// 				<h3 style={{textAlign: "center"}}>{nodeDatum.name}</h3>
+// 			</div>
+// 		</foreignObject>
+// 	</g>
+// );
+
+// centers the tree
+export const useCenteredTree = (defaultTranslate = {x: 0, y: 0}) => {
+	const [translate, setTranslate] = useState(defaultTranslate);
+	const containerRef = useCallback((containerElem) => {
+		if (containerElem !== null) {
+			const {width, height} = containerElem.getBoundingClientRect();
+			setTranslate({x: width / 2, y: height / 2});
 		}
-	],
-	"refs": [
-		{
-			"name": "main",
-			"hash": "7c570dce251232eecd2daa6bd81723ef0a1a7590"
+	}, []);
+	return [translate, containerRef];
+};
+
+async function getQueryString() {
+	let queryString = window.location.search
+	let queryStringPromise = new Promise((resolve) => {
+		if (queryString != undefined) {
+			resolve(queryString)
 		}
-	]
+	})
+	return queryStringPromise
 }
+
 export default function HistoryPage() {
-	const [historyTree, setHistoryTree] = useState(testobj);
+	const [historyTree, setHistoryTree] = useState({});
 	const [translate, containerRef] = useCenteredTree();
 	const textLayout = {attribute: {dy: "5em", x: 200000}};
 	const nodeSize = {x: 200, y: 200};
-	const foreignObjectProps = {width: nodeSize.x, height: nodeSize.y, x: 20};
+	const [queryString, setQueryString] = useState("1".repeat(32))
+	useLayoutEffect(() => {
+		getQueryString()
+			.then(data => {
 
-	useEffect(() => {
-		getHistoryAndMakeTree()
-			.then(tree => {
-				setHistoryTree(tree)
+				const urlParams = new URLSearchParams(data)
+				const fileID = urlParams.get('id')
+				getHistoryAndMakeTree(fileID)
+					.then(tree => {
+						setHistoryTree(tree)
+					}
+
+					)
 			}
-
 			)
-	}, [])
+	})
+	// const foreignObjectProps = {width: nodeSize.x, height: nodeSize.y, x: 20};
 
-	return (
 
 
-		<div className={style.fullscreen} ref={containerRef} id="treeWrapper" >
-			<Tree
-				data={historyTree}
-				translate={translate}
-				draggable={true}
-				onNodeClick={logNode}
-				collapsible={false}
-				rootNodeClassName={style.node__root}
-				branchNodeClassName={style.node__branch}
-				nodeSize={nodeSize}
-				textLayout={textLayout}
-				// renderCustomNodeElement={(rd3tProps) =>
-				// 	renderForeignObjectNode({...rd3tProps, foreignObjectProps})
-				// }
-				leafNodeClassName={style.node__leaf} />
-		</div>
+	if (historyTree != null) {
 
-	)
+		return (
+
+			<div className={style.fullscreen} ref={containerRef} id="treeWrapper" >
+				<Tree
+					data={historyTree}
+					translate={translate}
+					draggable={true}
+					onNodeClick={logNode}
+					collapsible={false}
+					rootNodeClassName={style.node__root}
+					branchNodeClassName={style.node__branch}
+					nodeSize={nodeSize}
+					textLayout={textLayout}
+					// renderCustomNodeElement={(rd3tProps) =>
+					// 	renderForeignObjectNode({...rd3tProps, foreignObjectProps})
+					// }
+					leafNodeClassName={style.node__leaf} />
+			</div>
+
+		)
+	} else {
+		return (
+
+			<div id="404div">
+
+				<h1>Something went wrong! Please go home and try again</h1>
+				<Link id="HomePageLink" href="/">Home</Link>
+			</div>
+		)
+	}
+
 
 }
