7b5ff607e365a1ddb478251c4328e7f003048842
cadencorontzos <60051669+cadencorontzos@users.noreply.github.com>
2023-05-03 15:22:28 -0700

    updated cypress unit tests after UI changes

--- cypress/components/indexHome.cy.js
+++ cypress/components/indexHome.cy.js
@@ -2,18 +2,11 @@ import React from 'react'
 import Home from '../../src/pages/index'
 
 describe('<Home />', () => {
-  it('render and displays expected content', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Home />)
-	
-	cy.get('p').contains("Hello World! Press buttons to see API calls in console")
+	it('render and displays expected content', () => {
+		// see: https://on.cypress.io/mounting-react
+		cy.mount(<Home />)
 
-	  cy.get('[id=listFiles]').should('be.visible')
-	  cy.get('[id=createFile]').should('be.visible')
-	  cy.get('[id=saveFile]').should('be.visible')
-	  cy.get('[id=getPreview]').should('be.visible')
-	  cy.get('[id=getFile]').should('be.visible')
-	  cy.get('[id=getHistory]').should('be.visible')
-	  cy.get('[id=historyPageLink]').should('be.visible')
-  })
+		cy.get('p').contains("Welcome to RemoteText")
+
+	})
 })
