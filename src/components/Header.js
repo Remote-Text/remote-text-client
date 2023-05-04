166a6d21a4613877ef388403a8733c847c254d0a
cadencorontzos <60051669+cadencorontzos@users.noreply.github.com>
2023-05-03 21:55:33 -0700

    added help icon to toolbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styles from "../styles/Home.module.css"
+import Link from 'next/link'
 
 const Header = () => {
 	return (
 		<div className={styles.imageHeader}>
-			<img src="/logo.png" alt="my_Logo"></img>
+			<Link href="/files">
+
+				<img src="/logo.png" alt="my_Logo"></img>
+			</Link>
+			<div class={styles.helpMenu} >
+				<button>?</button>
+			</div>
+
 		</div>
 	);
 };
