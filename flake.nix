b90e2bcdaf7d11fcad633cdd93734f4cd6dc6fe6
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-01-07 10:11:15 +0100

    Add Docker image

--- flake.nix
+++ flake.nix
@@ -7,6 +7,15 @@
         nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed
         (system: gen nixpkgs.legacyPackages.${system});
     in {
-      packages = forAllSystems (pkgs: { default = pkgs.callPackage ./. { }; });
+      packages = forAllSystems (pkgs: rec {
+        remote-text-web-client = pkgs.callPackage ./. { };
+        default = remote-text-web-client;
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "remote-text-web-client";
+          config = {
+            Cmd = [ "${remote-text-web-client}/bin/remote-text-web-client" ];
+          };
+        };
+      });
     };
 }
